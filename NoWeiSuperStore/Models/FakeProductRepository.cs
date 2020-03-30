06778038f87467b7cbb7d510f8087cc9d3018302
Wei Zhang <wei.zhang4@outlook.com>
2020-03-29 21:23:52 -0700

    Add project files.

---
+++ NoWeiSuperStore/Models/FakeProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoWeiSuperStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product> {
+        new Product { Name = "Wei's Signature Weaponized Football", Price = 25 },
+        new Product { Name = "Mithril Rocket Propelled Surf board", Price = 179 },
+        new Product { Name = "FeatherLite Bounding Running shoes", Price = 95 }
+        }.AsQueryable<Product>();
+    }
+}
