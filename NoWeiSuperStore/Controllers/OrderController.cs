0d2a570b0e34fd149b377263324f80f08828256f
Wei Zhang <wei.zhang4@outlook.com>
2020-04-06 09:49:37 -0700

    Added form for order processing.

---
+++ NoWeiSuperStore/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NoWeiSuperStore.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NoWeiSuperStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            public ViewResult Checkout() => View(new Order());
+        }
+    }
+}
