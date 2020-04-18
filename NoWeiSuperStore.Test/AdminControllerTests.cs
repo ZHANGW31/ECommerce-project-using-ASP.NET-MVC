a080ebbf13415c349c6c7cbf372924acd7bfcec6
Wei Zhang <wei.zhang4@outlook.com>
2020-04-18 15:02:50 -0700

    Created AdminControllerTests.cs

---
+++ NoWeiSuperStore.Test/AdminControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NoWeiSuperStore.Controllers;
+using NoWeiSuperStore.Models;
+using Xunit;
+
+namespace NoWeiSuperStore.Test
+{
+    class AdminControllerTests
+    {
+    }
+}
