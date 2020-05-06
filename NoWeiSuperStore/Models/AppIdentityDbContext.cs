cb35a46f47456cd0d429797df0f8fcf55b6fa500
Wei Zhang <wei.zhang4@outlook.com>
2020-05-06 08:53:02 -0700

    Removed AppIdentityObjectDbContext, and replaced it with AppIdentityDbContext.

--- NoWeiSuperStore/Models/AppIdentityDbContext.cs
+++ NoWeiSuperStore/Models/AppIdentityDbContext.cs
@@ -7,6 +7,6 @@ namespace NoWeiSuperStore.Models
     public class AppIdentityDbContext : IdentityDbContext<IdentityUser>
     {
         public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
-        : base(options) { }
+            : base(options) { }
     }
 }
